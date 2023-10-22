7fc4a9d8498ef948d09fbe408d9c8abe401aa7bf
Johanna Fransson <adrina0002@gmail.com>
2023-10-21 22:30:41 -0500

    use loggedInHook in createcomment

--- src/components/forms/CreateComment.tsx
+++ src/components/forms/CreateComment.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { z, ZodError } from "zod";
 import { getCookie } from "@/helper";
+import useGetLoggedInUser from "@/hooks/useGetLoggedInUser";
 
 const postSchema = z.object({
   content: z.string().min(1, { message: "Content is required" }),
@@ -19,33 +20,11 @@ interface Props {
   postId: number;
 }
 
-interface UserData {
-  username: string;
-  vaultId: string;
-}
-
-interface Community {
-  group_id: string;
-  user_community_id: number;
-  vault_id: string;
-}
-
 function CreatePost({ postId }: Props) {
   const router = useRouter();
-  const [userData, setUserData] = useState<UserData | null>(null);
   const [isCreatingComment, setIsCreatingComment] = useState(false);
   const { control, handleSubmit, reset } = useForm<PostForm>();
-
-  useEffect(() => {
-    const loggedInUser = getCookie();
-
-    if (loggedInUser) {
-      setUserData({
-        username: loggedInUser.username,
-        vaultId: loggedInUser.vault_id,
-      });
-    }
-  }, []);
+  const { loggedInUser } = useGetLoggedInUser();
 
   const handleCreateComment = async (data: PostForm) => {
     try {
@@ -58,7 +37,7 @@ function CreatePost({ postId }: Props) {
       const comment = await ApiService.createComment(
         data.content,
         postId,
-        userData?.vaultId || ""
+        loggedInUser?.vault_id || ""
       );
       console.log("Comment created:", comment);
 
